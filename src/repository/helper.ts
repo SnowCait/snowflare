cb486591bec297169560acfa79bac67ffc509571
SnowCait <SnowCait@users.noreply.github.com>
2025-01-13 04:20:38 +0900

    Refactoring

--- src/repository/helper.ts
+++ src/repository/helper.ts
@@ -1,6 +1,10 @@
-import { Event } from "nostr-tools";
+import { Event, Filter } from "nostr-tools";
 
 export const hexRegExp = /^[0-9a-z]{64}$/;
+
+export const idsFilterKeys: string[] = ["ids", "limit"] satisfies Array<
+  keyof Pick<Filter, "ids" | "limit">
+>;
 
 export function reverseChronological(x: Event, y: Event): number {
   return y.created_at - x.created_at;
--- src/repository/kv/d1/event.ts
+++ src/repository/kv/d1/event.ts
@@ -2,7 +2,7 @@ import { Event, Filter } from "nostr-tools";
 import { EventRepository } from "../../event";
 import { Bindings } from "../../../app";
 import { nip11 } from "../../../config";
-import { hexRegExp, reverseChronological } from "../../helper";
+import { hexRegExp, idsFilterKeys, reverseChronological } from "../../helper";
 
 export class KvD1EventRepository implements EventRepository {
   #env: Bindings;
@@ -21,36 +21,37 @@ export class KvD1EventRepository implements EventRepository {
   }
 
   async find(filter: Filter): Promise<Event[]> {
-    const idsFilterKeys: string[] = ["ids", "limit"] satisfies Array<
-      keyof Pick<Filter, "ids" | "limit">
-    >;
     if (
       Object.keys(filter).every((key) => idsFilterKeys.includes(key)) &&
       Array.isArray(filter.ids) &&
       filter.ids.length <= nip11.limitation.max_limit
     ) {
-      const events = await Promise.all(
-        filter.ids
-          .filter((id) => hexRegExp.test(id))
-          .map(async (id) => {
-            try {
-              const json = await this.#env.events.get(id);
-              if (json === null) {
-                return null;
-              }
-              return JSON.parse(json) as Event;
-            } catch (error) {
-              console.error("[json parse failed]", error);
-              return null;
-            }
-          }),
-      );
-      return events
-        .filter((event) => event !== null)
-        .toSorted(reverseChronological);
+      return this.#findByIds(filter.ids);
     } else {
       // TODO
       return [];
     }
+  }
+
+  async #findByIds(ids: string[]): Promise<Event[]> {
+    const events = await Promise.all(
+      ids
+        .filter((id) => hexRegExp.test(id))
+        .map(async (id) => {
+          try {
+            const json = await this.#env.events.get(id);
+            if (json === null) {
+              return null;
+            }
+            return JSON.parse(json) as Event;
+          } catch (error) {
+            console.error("[json parse failed]", error);
+            return null;
+          }
+        }),
+    );
+    return events
+      .filter((event) => event !== null)
+      .toSorted(reverseChronological);
   }
 }
