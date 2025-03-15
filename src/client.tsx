6a31ed76b86637db9bdd164b9e20e8c2f4224518
SnowCait <SnowCait@users.noreply.github.com>
2025-03-15 12:29:42 +0900

    Terms of Service and Privacy Policy

--- src/client.tsx
+++ src/client.tsx
@@ -19,12 +19,39 @@ app.get("/", (c) => {
 
       <body>
         <main>
-          <h1>Snowflare</h1>
-
           <section>
+            <h1>Snowflare</h1>
+            <p>This is a Nostr relay which requires registration to write.</p>
+            <p>
+              By registering, you are deemed to have agreed to the Terms of
+              Service and Privacy Policy.
+            </p>
             <button id="register">Register</button>
             <p id="registered" hidden>
               Registered.
+            </p>
+          </section>
+          <section>
+            <h3>Terms of Service</h3>
+            <p>
+              This service is provided as is without warranty of any kind.
+              <br />
+              The administrator does not take any responsibility.
+              <br />
+              The administrator may delete data or restrict access at their
+              discretion.
+              <br />
+              In addition to illegal activities, nuisance activities such as
+              spam are also prohibited.
+            </p>
+            <h3>Privacy Policy</h3>
+            <p>
+              Transmitted data and sender information (such as the IP address)
+              will be stored.
+              <br /> Transmitted data will be made public. If encrypted, it will
+              be made public in its encrypted state.
+              <br /> Sender information will not be made public, but may be
+              provided to the police, etc. if necessary.
             </p>
           </section>
         </main>
